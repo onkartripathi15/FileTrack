514d2ee8233efb17af8c04da51e694d7da445ccc
onkartripathi15 <tripathi.onkar@thinksys.com>
2022-07-04 18:36:59 +0530

    initial commit

---
+++ Mail Api/MailService.WebApi/DataContext/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using MailService.WebApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MailService.WebApi.DataContext
+{
+    class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=CPU-0191; Initial Catalog=config;Trusted_Connection=True");
+        }
+        public DbSet<Mail> path { get; set; }
+
+        public DbSet<User> users { get; set; }  
+    }
+}
---
+++ Mail Api/MailService.WebApi/Services/MailService.cs
@@ -0,0 +1,55 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using MailService.WebApi.DataContext;
+using MailService.WebApi.Models;
+using MailService.WebApi.Settings;
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.Extensions.Options;
+using MimeKit;
+using System;
+using System.IO;
+using System.Linq;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+using SmtpClient = MailKit.Net.Smtp.SmtpClient;
+
+namespace MailService.WebApi.Services
+{
+
+    public class access
+    {
+
+    }
+public class MailService : IMailService
+    {
+        private readonly MailSettings _mailSettings;
+        public MailService(IOptions<MailSettings> mailSettings)
+        {
+            _mailSettings = mailSettings.Value;
+        }
+
+
+  
+    public async Task SendEmailAsync(MailRequest mailRequest)
+        {
+
+            var email = new MimeMessage();
+            email.Sender = MailboxAddress.Parse(_mailSettings.Mail);
+            email.To.Add(MailboxAddress.Parse(mailRequest.ToEmail));
+            email.Subject = mailRequest.Subject;
+            string text = File.ReadAllText(@"C:\Users\thinksysuser\Desktop\Data\OldData");
+            var builder = new BodyBuilder();
+            builder.TextBody = text;
+            email.Body = builder.ToMessageBody();
+            using var smtp = new SmtpClient();
+            smtp.Connect(_mailSettings.Host, _mailSettings.Port, SecureSocketOptions.StartTls);
+            smtp.Authenticate(_mailSettings.Mail, _mailSettings.Password);
+            await smtp.SendAsync(email);
+
+            smtp.Disconnect(true);
+        }
+
+       
+    }
+}
